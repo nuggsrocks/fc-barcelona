fa2ad22c4c5628afbeff8508ebb9dda49b1a4fb5
mattfisher <nuggsrocks@yahoo.com>
2021-07-26 19:33:28 -0400

    test setup

---
+++ __tests__/loadTextFiles.test.js
@@ -0,0 +1,9 @@
+import { loadTextFiles } from '../src/js/loadTextFiles'
+
+import txt1 from '../src/text/history.txt'
+
+describe('loadTextFiles()', () => {
+  it('should load all files in given array asynchronously', () => {
+    loadTextFiles()
+  })
+})
