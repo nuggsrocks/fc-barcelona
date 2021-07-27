2acad11dc833b1ff1b48d476a3e0217f0150b3b4
mattfisher <nuggsrocks@yahoo.com>
2021-07-26 20:00:35 -0400

    loadTextFiles testing

--- __tests__/loadTextFiles.test.js
+++ __tests__/loadTextFiles.test.js
@@ -1,9 +1,27 @@
 import { loadTextFiles } from '../src/js/loadTextFiles'
 
 import txt1 from '../src/text/history.txt'
+import txt2 from '../src/text/fans.txt'
 
 describe('loadTextFiles()', () => {
-  it('should load all files in given array asynchronously', () => {
-    loadTextFiles()
+  it('should load all files in given array asynchronously', async () => {
+    const mockFetch = () => {
+      return new Promise(resolve => {
+        resolve({
+          text: () => {
+            return new Promise(resolve => resolve('text'))
+          }
+        })
+      })
+    }
+
+    const files = [{name: 'history', url: txt1}, {name: 'fans', url: txt2}]
+
+    const loaded = await loadTextFiles(mockFetch, files)
+
+    for (const index in loaded) {
+      expect(loaded[index]).toHaveProperty('name', files[index].name)
+      expect(loaded[index]).toHaveProperty('text', 'text')
+    }
   })
 })
--- src/js/loadTextFiles.js
+++ src/js/loadTextFiles.js
@@ -1,3 +1,11 @@
-export const loadTextFiles = (files) => {
+export const loadTextFiles = async (fetch, files) => {
+  const loaded = []
 
+  for (const file of files) {
+    const res = await fetch(file.url)
+    const text = await res.text()
+    loaded.push({name: file.name, text})
+  }
+
+  return loaded
 }
