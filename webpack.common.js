07bb842f7691957a6a0a17bacc58297024d725b6
mattfisher <nuggsrocks@yahoo.com>
2022-02-25 01:34:52 -0500

    rename webpack output

--- webpack.common.js
+++ webpack.common.js
@@ -4,8 +4,8 @@ const path = require('path')
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
   },
   module: {
     rules: [
